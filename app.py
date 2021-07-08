e31ab870c1e1a9d6a5bb62675f1619fac673d0f2
bwalters13 <34872925+bwalters13@users.noreply.github.com>
2021-07-08 14:48:57 -0700

    add league leaders page

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ index_page = html.Div(children=[
         html.Img(src=svg),
         dcc.Link('Per 36 stats', href='/per36'),
         dcc.Link('Advanced Stats', href='/advanced'),
+        dcc.Link('League Leaders', href='/league_leaders')
     ])
 ]),
 
@@ -54,6 +55,7 @@ per36_layout = html.Div(children=[
         html.Img(src=svg),
         dcc.Link('Per 36 stats', href='/per36'),
         dcc.Link('Advanced Stats', href='/advanced'),
+        dcc.Link('League Leaders', href='/league_leaders')
     ]),
     html.H4(children='Per 36 Stats'),
     dcc.Dropdown(
@@ -95,6 +97,7 @@ advanced_layout = html.Div(children=[
         html.Img(src=svg),
         dcc.Link('Per 36 stats', href='/per36'),
         dcc.Link('Advanced Stats', href='/advanced'),
+        dcc.Link('League Leaders', href='/league_leaders')
     ]),
     html.H4(children='Advanced Stats'),
     dcc.Dropdown(
@@ -134,6 +137,116 @@ advanced_layout = html.Div(children=[
     ),
 ])
 
+ll_layout = html.Div(children=[
+    html.Div(className='topnav', children=[
+        html.Img(src=svg),
+        dcc.Link('Per 36 stats', href='/per36'),
+        dcc.Link('Advanced Stats', href='/advanced'),
+        dcc.Link('League Leaders', href='/league_leaders')
+    ]),
+    html.H3("Select Minutes Qualifier"),
+    dcc.Slider(
+        className="slider",
+        id="qualifier",
+        min=0,
+        max= int(adv_df["MIN_player"].max())+1,
+        step=20,
+        marks={ i:str(i) for i in range(0, int(adv_df["MIN_player"].max())+1, 100)},
+        value=100,
+    ), 
+    html.H2("True Shooting Leaders"),
+    dash_table.DataTable(
+        id='test',
+        columns=[{'name': i, 'id': i} for i in ['Player','team','TS%']],
+        style_cell={'minWidth': '40px', 'width': '40px', 'maxWidth': '40px', 'whitespace': 'normal'},
+        style_table={
+            'width': "50%",
+            'margin': 'auto'
+        }
+    ),
+    html.Br(),
+    html.H2("Usage Leaders"),
+    dash_table.DataTable(
+        id='usage-table',
+        columns=[{'name': i, 'id': i} for i in ['Player','team','Usage']],
+        style_cell={'minWidth': '40px', 'width': '40px', 'maxWidth': '40px', 'whitespace': 'normal'},
+        style_table={
+            'width': "50%",
+            'margin': 'auto'
+        }
+    ),
+    html.Br(),
+    html.H2("Floor Percentage Leaders"),
+    dash_table.DataTable(
+        id='floor-table',
+        columns=[{'name': i, 'id': i} for i in ['Player','team','Floor Percentage']],
+        style_cell={'minWidth': '40px', 'width': '40px', 'maxWidth': '40px', 'whitespace': 'normal'},
+        style_table={
+            'width': "50%",
+            'margin': 'auto'
+        }
+    ),
+    html.Br(),
+    html.H2("Scoring Possessions Leaders"),
+    dash_table.DataTable(
+        id='sposs-table',
+        columns=[{'name': i, 'id': i} for i in ['Player','team','Scoring Possessions']],
+        style_cell={'minWidth': '40px', 'width': '40px', 'maxWidth': '40px', 'whitespace': 'normal'},
+        style_table={
+            'width': "50%",
+            'margin': 'auto'
+        }
+    ),
+    html.Br(),
+    html.H2("Possessions Leaders"),
+    dash_table.DataTable(
+        id='poss-table',
+        columns=[{'name': i, 'id': i} for i in ['Player','team','Possessions']],
+        style_cell={'minWidth': '80px', 'width': '80px', 'maxWidth': '80px', 'whitespace': 'normal'},
+        style_table={
+            'width': "50%",
+            'margin': 'auto'
+        }
+    ),
+])
+
+@app.callback(
+    Output('poss-table', 'data'),
+    Input('qualifier', 'value')
+)
+def get_usage_leaders(min_tsa):
+    return adv_df.loc[adv_df['MIN_player'] >= min_tsa, ['Player','team','Possessions']].sort_values('Possessions', ascending=False)[:10].to_dict('records')
+
+@app.callback(
+    Output('sposs-table', 'data'),
+    Input('qualifier', 'value')
+)
+def get_usage_leaders(min_tsa):
+    return adv_df.loc[adv_df['MIN_player'] >= min_tsa, ['Player','team','Scoring Possessions']].sort_values('Scoring Possessions', ascending=False)[:10].to_dict('records')
+
+@app.callback(
+    Output('floor-table', 'data'),
+    Input('qualifier', 'value')
+)
+def get_usage_leaders(min_tsa):
+    return adv_df.loc[adv_df['MIN_player'] >= min_tsa, ['Player','team','Floor Percentage']].sort_values('Floor Percentage', ascending=False)[:10].to_dict('records')
+
+
+@app.callback(
+    Output('usage-table', 'data'),
+    Input('qualifier', 'value')
+)
+def get_usage_leaders(min_tsa):
+    return adv_df.loc[adv_df['MIN_player'] >= min_tsa, ['Player','team','Usage']].sort_values('Usage', ascending=False)[:10].to_dict('records')
+
+@app.callback(
+    Output('test', 'data'),
+    Input('qualifier', 'value')
+)
+def get_df_qual(min_tsa):
+    val = "You have selected " + str(min_tsa)
+    return adv_df.loc[adv_df['MIN_player'] >= min_tsa, ['Player','team','TS%']].sort_values('TS%', ascending=False)[:10].to_dict('records')
+
 @app.callback(
     Output('advanced-table', 'columns'),
     Output('advanced-table', 'data'),
@@ -165,6 +278,8 @@ def display_page(pathname):
         return per36_layout
     elif pathname == '/advanced':
         return advanced_layout
+    elif pathname == '/league_leaders':
+        return ll_layout
     else:
         return index_page
 
