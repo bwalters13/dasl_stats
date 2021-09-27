70b1d67d55136d17b4b55b66a65492d5896a1423
bwalters13 <34872925+bwalters13@users.noreply.github.com>
2021-09-27 10:31:18 -0700

    update stats, new season

--- app.py
+++ app.py
@@ -48,14 +48,14 @@ team_colors = {
 }
 
 
-df = pd.read_csv('89season.csv')
+df = pd.read_csv('90season.csv')
 df_team = df.groupby('team').sum()
 df_team['num_games'] = df.groupby('team')['game_id'].nunique()
 for x in df_team.columns[1:14]:
     df_team[x] = (df_team[x]/df_team['num_games']).round(2)
 df_team_avg = df_team[df_team.columns[1:14]].reset_index()
 per36 = df.columns[3:16].tolist()
-adv_df = pd.read_csv('89advanced.csv')
+adv_df = pd.read_csv('90advanced.csv')
 min_limit = math.ceil(adv_df['MIN_player'].describe()['25%']/10)*10
 
 important_cols = ['Player'] + df.columns[2:16].tolist()
