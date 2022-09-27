8be9f55d7ae63776e85cb1efa8c4ce322a409ea6
bwalters13 <blakewalterss@gmail.com>
2022-09-27 13:29:27 -0700

    new season

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import base64
 import numpy as np
 import math
 
-season = 97
+season = 98
 svg_file = "demo.svg"
 encoded = base64.b64encode(open(svg_file,'rb').read())
 svg = 'data:image/svg+xml;base64,{}'.format(encoded.decode())
